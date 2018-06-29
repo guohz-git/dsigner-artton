8af15d7a031a938106f34b5134a648bd8561b9a0
guoyu <351150710@qq.com>
2018-06-29 17:53:03 +0800

    ...

---
+++ src/businessDelegate/BusinessService.java
@@ -0,0 +1,5 @@
+package businessDelegate;
+
+public interface BusinessService {
+    public void doProcessing();
+}
---
+++ src/businessDelegate/Client.java
@@ -0,0 +1,16 @@
+package businessDelegate;
+
+public class Client {
+
+    BusinessDelegate businessDelegate;
+
+    public Client(BusinessDelegate businessDelegate) {
+        this.businessDelegate = businessDelegate;
+    }
+
+    public void doTask(){
+        businessDelegate.doTask();
+    }
+
+
+}
